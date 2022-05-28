bf88f9958d3994b025afb976c8bb8561f296c875
Jonathan Bridge <jvbridge@gmail.com>
2022-05-28 11:52:21 -0700

    create new user

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -8,6 +8,16 @@ router.get("/", (req, res) => {
   }
 });
 
+router.post("/", (req, res) => {
+  try {
+    // TODO: send user information
+    //TODO: express session
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post("/login", (req, res) => {
   try {
     // TODO: express session
