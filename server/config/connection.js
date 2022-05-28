84bfbdf3925b2f0001e25046c749e007854c501f
Jonathan Bridge <jvbridge@gmail.com>
2022-05-28 11:45:30 -0700

    changed db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // Wrap Mongoose around local connection to MongoDB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/departmentsDB",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/midnightDB",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
