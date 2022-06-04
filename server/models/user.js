a145b236f95cdb8bdee937ff50da4caa8a578d76
Jonathan Bridge <jvbridge@gmail.com>
2022-06-04 10:40:20 -0700

    virtuals for password encryption

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // checks the password
 const checkPassword = (loginPw, userPw) => {
@@ -10,22 +11,44 @@ const createPassword = async (newPw) => {
   return newHash;
 };
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    maxlength: 50,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      maxlength: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    maxlength: 50,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// has the password every time we add a user or change their password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    // 10 rounds by default
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
 });
 
-const User = model("user", userSchema);
+// comparison for loggin in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model("User", userSchema);
 model.exports = User;
