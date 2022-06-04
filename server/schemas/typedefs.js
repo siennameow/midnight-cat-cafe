ffe9c4eb079bf294d96fee0391b92a751f5b91c9
Jonathan Bridge <jvbridge@gmail.com>
2022-06-04 14:13:56 -0700

    typedefs

---
+++ server/schemas/typedefs.js
@@ -0,0 +1,36 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String
+    email: String!
+    password: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Event {
+    users: [User]
+    time: Date!
+    name: String!
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addEvent(title: String!, time: Date!): Event
+    # TODO: remove user, event, add user to event
+  }
+`;
+
+module.exports = typeDefs;
