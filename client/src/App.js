b98c597d6f5a15f29201ddb95790d4a7b99693de
luccaloopz <luccamartins2012@gmail.com>
2022-06-04 13:26:06 -0700

    App.js on client side is set up to route to different pages/components using the react-router-dom package

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Events from './pages/Events';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Register from './pages/Register';
+import Shopping from './pages/Shopping';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+              <Route 
+                path='/'
+                element={<Home />} 
+              />
+              <Route 
+                path='/register'
+                element={<Register />} 
+              />
+              <Route 
+                path='/shopping'
+                element={<Shopping />} 
+              />
+              <Route 
+                path='/menu'
+                element={<Menu />} 
+              />
+              <Route 
+                path='/events'
+                element={<Events />} 
+              />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
