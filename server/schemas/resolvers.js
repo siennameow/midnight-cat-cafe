c2bd7dc4c1e5d786fc4b6a5ec366e817a6f8117f
Jonathan Bridge <jvbridge@gmail.com>
2022-06-04 19:27:21 -0700

    stub code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,15 @@
 const { signToken } = require("../util/auth");
 const { User, Event } = require("../models");
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    users: async () => {
+      User.find().populate(events);
+    },
+    user: async (parent, { username }) => {}, // TODO: complete these yo!
+    me: User,
+  },
+  Mutation: {},
+};
 
 module.exports = resolvers;
