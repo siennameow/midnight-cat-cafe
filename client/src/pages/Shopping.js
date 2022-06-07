2d5d5bf1db7707b336fdb6cfe70a950130beb226
Hexuan Li <lihexuan1@gmail.com>
2022-06-06 21:28:58 -0700

    create shopping page components

---
+++ client/src/pages/Shopping.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ShoppingCard from '../components/Shopping/ShoppingCard';
+
+function Shopping() {
+    return (
+      <div>
+        <h1 className='text-center' style={{color: '#a16b1b'}}>Shopping for Midnight Cat products</h1>
+      <div  className="d-flex row row-cols-1 row-cols-md-2 row-cols-xl-4 g-4">
+      <ShoppingCard />
+      </div>
+      </div>
+    );
+  }
+
+
+export default Shopping;
