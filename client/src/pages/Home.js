1abd2508051e619194ac98defa1ef3ec652c6e3b
Hexuan Li <lihexuan1@gmail.com>
2022-06-06 21:42:12 -0700

    create home page component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Carousels from "./Carousel"
+
+
+function Home() {
+  return (
+    <div>
+    <h1 className='text-center' style={{color: '#a16b1b'}}>Midnight Cat Cafe is a dream place for you!</h1>
+    <Carousels />
+    <p>We are Midnight Cat Cafe. We are Midnight Cat Cafe. We are Midnight Cat Cafe. We are Midnight Cat Cafe. We are Midnight Cat Cafe. We are Midnight Cat Cafe. We are Midnight Cat Cafe. We are Midnight Cat Cafe. We are Midnight Cat Cafe. We are Midnight Cat Cafe. We are Midnight Cat Cafe.</p>
+    </div>
+  )
+}
+
+export default Home
