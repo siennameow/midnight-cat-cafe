b4006a228a14c114c8af8960b8d37dc985155f4d
Hexuan Li <lihexuan1@gmail.com>
2022-06-06 21:40:36 -0700

    create carousels components

---
+++ client/src/components/Home/Carousel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+
+function Carousels() {
+    return(
+        <Carousel>
+
+    </Carousel>
+    )
+}
+
+export default Carousels
