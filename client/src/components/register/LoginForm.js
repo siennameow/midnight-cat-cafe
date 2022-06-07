b4300310a28e6ef156c9680260bac56a144e7b20
luccaloopz <luccamartins2012@gmail.com>
2022-06-07 13:28:08 -0700

    created inital Navbar functionality

---
+++ client/src/components/register/LoginForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+const LoginForm = () => {
+
+  return (
+    <Form>
+      <Form.Group>
+        <Form.Label>Email</Form.Label>
+        <Form.Control 
+          type='text'
+          placeholder='Your email'
+          name='email'
+        />
+      </Form.Group>
+    </Form>
+  );
+};
+
+export default LoginForm;
