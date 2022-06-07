b4d7088b0f01555ee509d17211f8dec000eb7836
Hexuan Li <lihexuan1@gmail.com>
2022-06-06 21:56:27 -0700

    add json data to event page components

--- client/src/components/Event/EventCard.js
+++ client/src/components/Event/EventCard.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React from 'react';
+import EventCard from './EventCard';
+const events= [
+  {
+    id: 1,
+    src: 'https://cdn.filestackcontent.com/compress/output=format:webp/cache=expiry:max/resize=width:936/d72jt183TkGNmXDIkqst',
+    title: 'Paint Night',
+    description: 'Enjoy the our special drinks while painting',
+    time: 'Saturday 7:00-8:00pm Aug 26th, 2022'
+  },
+  {
+    id: 2,
+    src: 'https://insidethemagic.net/wp-content/uploads/2020/04/@Baristart.png',
+    title: 'Latte Art Class',
+    description: 'Learn the basic skill for making a latte art from zero',
+    time: 'Saturday 2:00-3:00pm July 25th, 2022'
+  },
+  {
+    id: 3,
+    src: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/party-cat-sunglasses-cool-fun-event-disco-ad-design-template-f82bff1a6f7ee7bace485f82d2cbe5db_screen.jpg?ts=1610722037',
+    title: 'Dance With Cats',
+    description: 'Bring your cats to the cafe and enjoy this Friday night dancing party.',
+    time: 'Friday 7:00-8:00pm Aug 13th, 2022'
+  },
+]
 
-function EventCard (props) {
-return (
-    props.events.map(list => 
-<div className="card mb-3 text-center g-4">
-  <img src={list.src} className="card-img text-center" alt={list.title} style={{width:'100%'}}/>
-  <div className="card-body">
-    <h5 className="card-title text-dark">{list.title}</h5>
-    <p className="card-text text-dark">{list.description}</p>
-    <p className="card-text text-dark">{list.time}</p>
-    <a href="/" className="btn btn-warning">See event detail</a>
-  </div>
-</div>
-))}
-
-export default EventCard;
+function Event() {
+  return (
+    <div>
+      <h1 className='text-center' style={{color: '#a16b1b'}}>Upcoming Event</h1>
+    <div className='px-8'>
+    <EventCard events={events}/>
+    </div>
+    </div>
+  );
+}
+export default Event;
