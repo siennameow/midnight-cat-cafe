7771b1dbfbaf0736d2bf9254262d5e48f01e00eb
Jonathan Bridge <jvbridge@gmail.com>
2022-06-09 14:57:13 -0700

    you can now sign up

--- client/src/components/register/LoginForm.js
+++ client/src/components/register/LoginForm.js
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import { Form, Alert, Button } from 'react-bootstrap';
-// import { useMutation } from '@apollo/client';
-import '../register/login.css'
+import { useMutation } from '@apollo/client';
+import '../register/login.css';
 
-// import Auth from '../../utils/auth';
-// import { LOGIN_USER } from "../../utils/mutations";
+import Auth from '../../utils/auth';
+import { LOGIN } from '../../utils/mutations';
 
 const LoginForm = () => {
-  const [userLoginData, setUserLoginData] = useState({ email: '', password: '' });
+  const [userLoginData, setUserLoginData] = useState({
+    email: '',
+    password: '',
+  });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-  // const [login, { error }] = useMutation(LOGIN_USER);
+  const [login, { error }] = useMutation(LOGIN);
+
+  if (error) {
+    console.error(error);
+  }
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,16 +33,16 @@ const LoginForm = () => {
       event.stopPropagation();
     }
 
-    // try {
-    //   const { data } = await login({
-    //     variables: { ...userFormData }
-    //   });
+    try {
+      const { data } = await login({
+        variables: { ...userLoginData },
+      });
 
-    //   Auth.login(data.login.token);
-    // } catch (err) {
-    //   console.error(err);
-    //   setShowAlert(true);
-    // }
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
 
     setUserLoginData({
       username: '',
@@ -46,42 +53,53 @@ const LoginForm = () => {
 
   return (
     <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-      <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+      <Alert
+        dismissible
+        onClose={() => setShowAlert(false)}
+        show={showAlert}
+        variant="danger"
+      >
         Something went wrong with your login credentials!
       </Alert>
 
-      <Form.Group style={{marginBottom: "2%"}}>
-        <Form.Label htmlFor='email'>Email</Form.Label>
-        <Form.Control 
-          type='text'
-          placeholder='Your email'
-          name='email'
+      <Form.Group style={{ marginBottom: '2%' }}>
+        <Form.Label htmlFor="email">Email</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Your email"
+          name="email"
           onChange={handleInputChange}
           value={userLoginData.email}
           required
         />
-        <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+        <Form.Control.Feedback type="invalid">
+          Email is required!
+        </Form.Control.Feedback>
       </Form.Group>
 
-      <Form.Group style={{marginBottom: "2%"}}>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userLoginData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
+      <Form.Group style={{ marginBottom: '2%' }}>
+        <Form.Label htmlFor="password">Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Your password"
+          name="password"
+          onChange={handleInputChange}
+          value={userLoginData.password}
+          required
+        />
+        <Form.Control.Feedback type="invalid">
+          Password is required!
+        </Form.Control.Feedback>
+      </Form.Group>
       <center>
         <Button
           disabled={!(userLoginData.email && userLoginData.password)}
-          type='submit'
-          className='purple'>
+          type="submit"
+          className="purple"
+        >
           Login
-        </Button></center>
+        </Button>
+      </center>
     </Form>
   );
 };
