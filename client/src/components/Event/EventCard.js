bc4be7d4a63863518af8eb4c57b280d70c0dcf7b
Jonathan Bridge <jvbridge@gmail.com>
2022-06-09 20:42:54 -0700

    small refactor to make each event card its own component

--- client/src/components/Event/EventCard.js
+++ client/src/components/Event/EventCard.js
@@ -1,18 +1,27 @@
-import React from "react";
-import '../Event/event.css'
+import React from 'react';
+import '../Event/event.css';
 
-function EventCard (props) {
-return (
-    props.events.map(list => 
-<div className="card mb-3 text-center g-4">
-  <img src={list.src} className="card-img text-center" alt={list.title} style={{width:'100%'}}/>
-  <div className="card-body">
-    <h5 className="card-title text-dark">{list.title}</h5>
-    <p className="card-text text-dark"><i>{list.description}</i></p>
-    <p className="card-text text-dark">{list.time}</p>
-    <a href="/" className="btn btn-warning">See event detail</a>
-  </div>
-</div>
-))}
+function EventCard({ info }) {
+  return (
+    <div className="card mb-3 text-center g-4">
+      <img
+        src={info.src}
+        className="card-img text-center"
+        alt={info.title}
+        style={{ width: '100%' }}
+      />
+      <div className="card-body">
+        <h5 className="card-title text-dark">{info.title}</h5>
+        <p className="card-text text-dark">
+          <i>{info.description}</i>
+        </p>
+        <p className="card-text text-dark">{info.time}</p>
+        <a href="/" className="btn btn-warning">
+          See event detail
+        </a>
+      </div>
+    </div>
+  );
+}
 
 export default EventCard;
