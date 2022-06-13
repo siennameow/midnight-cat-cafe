7f20e13ea94a07dd584def1862744a49729d43df
Jonathan Bridge <jvbridge@gmail.com>
2022-06-13 15:16:20 -0700

    changed the path to refresh always

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // send the index.html file always
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
