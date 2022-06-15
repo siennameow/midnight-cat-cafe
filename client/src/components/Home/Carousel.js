4205a6233bb01ef6fe39afe6654be2559e4e07de
Hexuan Li <lihexuan1@gmail.com>
2022-06-15 12:37:35 -0700

    update carasel

--- client/src/components/Home/Carousel.js
+++ client/src/components/Home/Carousel.js
@@ -10,24 +10,26 @@ function Carousels() {
 
         <Carousel>
         <div>
-            <img src="https://images.unsplash.com/photo-1600093463592-8e36ae95ef56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="image1" />
+            <img src="https://images.xceed.me/restaurants/covers/aubaine-selfridges-restaurant-london-xceedcd8a.jpeg" alt="image1" />
             <p className="legend-1">Let us make your day!
             </p>
-        </div>   
+        </div>  
         <div>
-            <img src="https://media.istockphoto.com/photos/interior-of-cozy-caf-coffee-shop-small-business-at-counter-artisanal-picture-id1326498175?k=20&m=1326498175&s=612x612&w=0&h=d2xbNQ8Z58LoPphvppaBGAJbDoEn3oxaj4CZ3XZ7WD4=" alt="image1" />
+            <img src="https://coffeeaffection.com/wp-content/uploads/2020/10/Lavender-Coffee-latte_Shutterstock_Lyudmila-Mikhailovskaya.jpg" alt="image1" />
+            <p className="legend-1">Vegan-friendly.</p>
+        </div> 
+        <div>
+            <img src="https://res.cloudinary.com/simpleview/image/upload/v1648149199/clients/sanmateoca/Plate_of_food_at_a_restaurant_during_brunch_hours_26a1bed1-cc7d-487a-9f9b-5e3ae09035e5.jpg" alt="image1" />
             <p className="legend-1">A 100% plant-based cafe with non-dairy options available.</p>
         </div>
+
         <div>
-            <img src="https://media.istockphoto.com/photos/another-one-thank-you-picture-id1139917357?k=20&m=1139917357&s=612x612&w=0&h=8SqSPuRl6OGEF7rNJc4rIqt7zZp87z9kG7toloM2jF4=" alt="image1" />
-            <p className="legend-1">Connected. Organic. Alive. Natural.</p>
-        </div>
+            <img src="https://images.squarespace-cdn.com/content/v1/5418a5bae4b0877352524377/1457311229099-7630L329V1EVI1ISHEMT/image-asset.jpeg?format=1000w" alt="image1" />
+            <p className="legend-1">Connected. Organic. Alive. Natural.
+            </p>
+        </div>  
         <div>
-            <img src="https://media.istockphoto.com/photos/young-man-sitting-in-coffee-shop-and-using-laptop-picture-id1177375625?k=20&m=1177375625&s=612x612&w=0&h=_RmM1Xnv3ExaKMO6bYUS_-Sr1PlGz8ZQoXJHxRcbc4I=" alt="image1" />
-            <p className="legend-1">Vegan-friendly.</p>
-        </div>
-        <div>
-            <img src="https://www.westend61.de/images/0001180580pw/two-happy-young-women-with-cell-phone-meeting-in-a-cafe-AHSF00129.jpg" alt="image1" />
+            <img src="https://www.aimeeprovence.com.au/wp-content/uploads/2021/06/what-to-waer-to-high-tea.jpg" alt="image1" />
             <p className="legend-1">It’s the coffee that makes the neighborhood.</p>
         </div>
     </Carousel></center>
